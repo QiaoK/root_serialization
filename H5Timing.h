1853a734f25181f8cbad1e160a2c3f42401dcf75
Qiao Kang <qiaokang2015@u.northwestern.edu>
2022-04-09 14:26:16 -0700

    Update H5Timing.h

--- H5Timing.h
+++ H5Timing.h
@@ -59,13 +59,13 @@ int register_PDCwait_timer_end(double start_time);
 int increment_PDCwrite();
 int increment_PDCread();
 #else
-int register_merge_requests_timer_end(double start_time);
 int register_wrap_requests_timer_end(double start_time);
 int register_H5Dclose_timer_end(double start_time);
 int register_H5Dwrite_timer_end(double start_time);
 int increment_H5Dwrite();
 int increment_H5Dread();
 #endif
+int register_merge_requests_timer_end(double start_time);
 int register_dataset_timer_start(const char *name);
 int register_dataset_timer_end(size_t data_size);
 int register_dataset_sz_timer_start(const char *name);
